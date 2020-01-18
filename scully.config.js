09ecd27217b1d62cfc8e872686b422386926ff20
nishimura-yuji <nishimura.yuji@classmethod.jp>
2020-01-19 00:23:11 +0900

    fix: puppeteerLaunchOptionsを設定

--- scully.config.js
+++ scully.config.js
@@ -1,12 +1,13 @@
 exports.config = {
-  projectRoot: "./src/app",
+  projectRoot: './src/app',
+  puppeteerLaunchOptions: { args: ['--no-sandbox'] },
   outFolder: './dist/static',
   routes: {
     '/blog/:slug': {
       type: 'contentFolder',
       slug: {
-        folder: "./blog"
+        folder: './blog'
       }
-    },
+    }
   }
 };
