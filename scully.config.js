da3c59d2e49aa7bdf629ade9e181b224cb68e2f9
nishimura-yuji <nishimura.yuji@classmethod.jp>
2020-01-18 14:58:21 +0900

    add: ng g @scullyio/init:blog

--- scully.config.js
+++ scully.config.js
@@ -2,5 +2,11 @@ exports.config = {
   projectRoot: "./src/app",
   outFolder: './dist/static',
   routes: {
+    '/blog/:slug': {
+      type: 'contentFolder',
+      slug: {
+        folder: "./blog"
+      }
+    },
   }
 };
